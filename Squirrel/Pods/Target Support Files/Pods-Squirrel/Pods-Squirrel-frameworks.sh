beba241b22021cab969c256905595aa43385313b
Duc Nguyen <emngok123@gmail.com>
2017-05-16 11:14:59 +0700

    Remove the spare library

--- Squirrel/Pods/Target Support Files/Pods-Squirrel/Pods-Squirrel-frameworks.sh
+++ Squirrel/Pods/Target Support Files/Pods-Squirrel/Pods-Squirrel-frameworks.sh
@@ -85,9 +85,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/JFContactsPicker/JFContactsPicker.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/ZXingObjC/ZXingObjC.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/JFContactsPicker/JFContactsPicker.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/ZXingObjC/ZXingObjC.framework"
 fi
